6079ee400bacf8d19853d923cc32fc419a860d88
Keks <keks@htmlacademy.ru>
2025-11-23 21:54:45 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,5 @@
+import { drawThumbnails } from './thumbnails.js';
+import { initImageUpload } from './upload-form.js';
+
+drawThumbnails();
+initImageUpload();
---
+++ 11/js/validate.js
@@ -0,0 +1,57 @@
+function validate(imageUploadForm){
+  const hashtagField = imageUploadForm.querySelector('.text__hashtags');
+  const commentField = imageUploadForm.querySelector('.text__description');
+
+  const hashtagReg = /^#[a-zа-яё0-9]{1,19}$/i;
+  const MAX_HASHTAGS = 5;
+
+  const pristine = new Pristine(imageUploadForm);
+
+  imageUploadForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    pristine.validate();
+  });
+
+  function validateHashtags(hashtagsString) {
+    if (hashtagsString.trim() === '') {
+      return true;
+    }
+
+    const hashtags = hashtagsString.trim().split(/\s+/).filter((hashtag) => hashtag !== '');
+
+    if (hashtags.length > MAX_HASHTAGS) {
+      return false;
+    }
+
+    const seenHashtags = new Set();
+
+    for (const hashtag of hashtags) {
+      if (hashtag === '#') {
+        return false;
+      }
+
+      if (!hashtagReg.test(hashtag)) {
+        return false;
+      }
+
+      const lowerHashtag = hashtag.toLowerCase();
+      if (seenHashtags.has(lowerHashtag)) {
+        return false;
+      }
+      seenHashtags.add(lowerHashtag);
+    }
+
+    return true;
+  }
+
+  function validateComment(commentString) {
+    return commentString.length <= 140;
+  }
+
+  pristine.addValidator(hashtagField, validateHashtags, 'Некорректные хэштеги');
+  pristine.addValidator(commentField, validateComment, 'Максимум 140 символов');
+
+  return pristine;
+}
+
+export { validate };
